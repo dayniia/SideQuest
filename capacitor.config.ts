39e5495039acd25b76cd88dbb01c6b1deaca4c59
Dina <dynnf11@gmail.com>
2026-02-22 00:16:13 +0300

    add android capacitor support

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.sidequest.app',
+  appName: 'SideQuest',
+  webDir: 'dist'
+};
+
+export default config;
